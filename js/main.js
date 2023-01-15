0474676de0a8bd14b92c1d9c570be29acad956fd
SergioMack <badzega@gmail.com>
2023-01-15 20:24:09 +0100

    add_device_script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 "use strict";
 
-const isMobile = {
-    
-}
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        document.body.classList.add("_touches");
+} else {
+    document.body.classList.add('pc');
+};
