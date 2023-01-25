e9bc0f08a29125a2c0f905e3e17b43b8890df385
SergioMack <badzega@gmail.com>
2023-01-25 21:48:39 +0100

    second last

---
+++ js/cookies.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+// if(localStorage.getItem("cookiesAccepted") === null) {
+    setTimeout(() => {
+        let cookies = document.querySelector('#cookie_popup');
+        
+        cookies.classList.remove('d-none');
+        
+    }, 500);
+// }
+function closeCookies() {
+    let cookies = document.querySelector('#cookie_popup');
+   cookies.classList.add('d-none');
+    // localStorage.setItem("cookiesAccepted", true);
+}
+// if (localStorage.getItem("cookiesAccepted") === true) {
+//     loadAnalytics();
+//  }
