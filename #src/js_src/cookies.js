e823b4306bfce7bf8ca56503fd4ee3329ece4e6f
Sergio <badzega@gmail.com>
2023-05-01 20:23:55 +0200

    last commit

--- #src/js_src/cookies.js
+++ #src/js_src/cookies.js
@@ -1,18 +1,18 @@
 "use strict";
 
 
-// if(localStorage.getItem("cookiesAccepted") === null) {
+if(localStorage.getItem("cookiesAccepted") === null) {
     setTimeout(() => {
         let cookies = document.querySelector('#cookie_popup');
         
         cookies.classList.remove('d-none');
         
     }, 500);
-// }
+}
 function closeCookies() {
     let cookies = document.querySelector('#cookie_popup');
    cookies.classList.add('d-none');
-    // localStorage.setItem("cookiesAccepted", true);
+    localStorage.setItem("cookiesAccepted", true);
 }
 // if (localStorage.getItem("cookiesAccepted") === true) {
 //     loadAnalytics();
--- js/swiper.js
+++ js/swiper.js
@@ -7,25 +7,26 @@ const swiper = new Swiper('.swiper', {
   breakpoints: {
     400: {
       slidesPerView: 1,
-      spaceBetween: 5,
+      spaceBetween: 15,
     },
-    844: {
+    800: {
       slidesPerView: 3,
-      spaceBetween: 10,
+      spaceBetween: 25,
     },
     1024: {
       slidesPerView: 3,
-      spaceBetween: 40,
+      spaceBetween: 50,
     },
   },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    dynamicBullets: true,
     clickable: true,
   },
 
-  // Navigation arrows
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
