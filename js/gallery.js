fbf04b3ebef4582fa69910737c789af17e7be39c
Sergio_Mac <badzega@gmail.com>
2023-12-01 21:01:50 +0100

    ADD:transparent header

---
+++ js/gallery.js
@@ -0,0 +1,109 @@
+"use strict";
+
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+const popupLinks = document.querySelectorAll('.popup-link');  //wybór zdjęcia
+// const mainImage = document.querySelector('.popup_image_wrap'); // constanta dla wstawki img
+const popupImg = document.getElementById('popupImage');
+const popup = document.getElementById('popup');
+
+let unlock = true;
+const timeout = 800;
+
+if(popupLinks.length > 0){
+    for(let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener("click", function (e){
+            // const popupName = popupLink.getAttribute('src');
+            const curentPopup = document.getElementById('popup');
+            popup.classList.add('open');
+            popupImg.src = popupLink.src;
+            popupOpen(curentPopup);
+            e.preventDefault();
+        });
+    }
+}
+
+const popupCloseIcon = document.querySelector('.close-popup');
+
+if (popupCloseIcon > 0) {
+    for( let index = 0; index < popupCloseIcon.length; index++) {
+        const elClose = popupCloseIcon[index];
+        
+        elClose.addEventListener('click', function (e) {
+            popupClose(elClose.closest('.popup'));
+            // e.preventDefault();
+            // elClose.removeEventListener;
+
+            popupImg.addEventListener('ondbclick',function(e) { //zamekanie po dbckick
+                popupClose(elClose.closest('.popup'));
+                // e.preventDefault();
+                // popupImg.removeEventListener;
+                });
+
+
+        });
+    }
+}
+        
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
+        const popupActive = document.querySelector('.popup .open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
+        
+        curentPopup.classList.add('.open');
+        curentPopup.addEventListener("click", function (e) {
+            if (!e.target.closest('.popup-content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        });
+    }
+}
+
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove('open');
+        if (doUnlock) {
+            bodyUnLock();
+        }
+    }
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+    if (lockPadding.length > 0) {
+    for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = lockPaddingValue;
+        }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+function bodyUnLock() {
+    setTimeout(function() {
+        for(let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = '0px';
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock')
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function() {
+        unlock = true;
+    }, timeout);
+}
+
---
+++ sendmail.php
@@ -0,0 +1,70 @@
+<?php
+require'PHPMailer/src/Exception.php';
+require'PHPMailer/src/PHPMailer.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+
+
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('pl', 'PHPMailer/language/');
+$mail->isHTML(true);
+// $mail->isSMTP();
+// $mail->Host = 'localhost';
+// $mail->Port = 25;
+// ot kogo rezewacja
+$mail->setFrom('strona@sergioslab.pl', 'Serhii');
+// komu wysylamy
+$mail->addAddress('badzega@gmail.com');
+$mail->addAddress('katerynasukhovetska@gmail.com');
+// tema wiadomosci
+$mail->Subject = 'Kochanie ktos cos chce';
+
+// glowna cesc lista
+$body = '<h1>Zamowienie uslugi</h1>';
+
+if(!empty(trim($_POST['name']))){
+    $body.='<p><strong>Imie:</strong> '.trim($_POST['name']).'</p>';
+}
+if(trim(!empty($_POST['telefon']))){
+    $body.='<p><strong>telefon:</strong> '.$_POST['telefon'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['message']))){
+    $body.='<p><strong>Wiadomość:</strong> '.$_POST['message'].'</p>';
+}
+$selectedOption = $_POST['things_select'];
+
+
+$body .= '<p><strong>Zabiegi naRzesy + multi usługi:</strong> '.$selectedOption.'</p>';
+$body.='<p><strong>Zabiegi na Brwi:</strong> '.$_POST['brews_select'].'</p>';
+$body.='<p><strong>Data wybrana przez klienta:</strong> '.$_POST['calendar'].'</p>';
+
+
+$mail->Body = $body;
+
+// wysylamy forme
+if(!$mail->send()) {
+    $message = 'Blad1';
+} else {
+    $message = 'Wyslano';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+?>
+
+
+
+
+
