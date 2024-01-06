50be4cd3cf699dc85aa8e60e9871b749c0ab782a
Sergio_Mac <badzega@gmail.com>
2024-01-06 22:41:33 +0100

    one commit

--- #src/js_src/burger.js
+++ #src/js_src/burger.js
@@ -1,31 +1,28 @@
 "use strict";
 // wyznaczamy czy to jest urządzenie mobilne i dodajemy odpowiedni klas
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  document.body.classList.add("_touches");
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        document.body.classList.add("_touches");
 } else {
-  document.body.classList.add("_pc");
-}
+    document.body.classList.add("_pc");
+};
+
 
 // burger menu
 const menuBody = document.querySelector(".menu_body");
 const iconMenu = document.querySelector(".menu_icon");
-if (iconMenu) {
-  //sprawdzamy czy takie cos istnieje
-
-  iconMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle("_lock");
-    iconMenu.classList.toggle("_active");
-    menuBody.classList.toggle("_active");
-  });
+if (iconMenu) { //sprawdzamy czy takie cos istnieje
+    
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
 }
 
-const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
 
 if (menuLinks.length > 0) {
+<<<<<<< HEAD
   menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick);
   });
@@ -103,215 +100,32 @@ for (let i = 0; i < l; i++) {
         }
       }
       h.click();
+=======
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+>>>>>>> af022e5 (one commit)
     });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function (e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
-}
 
-function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box: */
-  var x,
-    y,
-    i,
-    xl,
-    yl,
-    arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (let i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i);
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (let i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const goToBlock = document.querySelector(menuLink.dataset.goto);
+            const goToBlockValue = goToBlock.getBoundingClientRect().top + pageYOffset - document.querySelector(".header").offsetHeight;
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
 
-//
-// otwieranie formy
-// відкриття форми
-// let isFormOpen = false;
-
-// // відкриття форми
-// function openForm() {
-//   document.querySelector("#formWrapper").classList.remove("d-none");
-//   isFormOpen = true;
-// }
-
-// const openLink = document.querySelector("#reservation");
-// openLink.addEventListener("click", openForm);
-
-// // закриття форми
-// const formWrapper = document.querySelector("#formWrapper");
-
-// document.addEventListener("click", function (e) {
-//   if (
-//     isFormOpen &&
-//     e.target !== formWrapper &&
-//     !formWrapper.contains(e.target)
-//   ) {
-//     closeForm();
-//   }
-// });
-
-let isFormOpen = false;
-let formWrapper = document.querySelector(".form_wrapper");
-let form = document.querySelector("#form");
-let closeBtn = document.querySelector("#form_close_btn");
-let openLink = document.querySelector("#reservation");
-let reservationBtn = document.querySelector("#reservation-btn");
-//
-// // відкриття форми
-function openForm() {
-  formWrapper.classList.toggle("d-none");
-  isFormOpen = true;
-}
-openLink.addEventListener("click", openForm);
-reservationBtn.addEventListener("click", () => {
-  if (!isFormOpen) {
-    openForm();
-  } else if (isFormOpen) {
-    closeForm();
-  }
-});
-
-// // закриття форми
-closeBtn.addEventListener("click", function (e) {
-  if (isFormOpen) {
-    closeForm();
-  }
-});
-
-function closeForm() {
-  formWrapper.classList.add("d-none");
-  isFormOpen = false;
-}
-
-document.addEventListener("click", function (e) {
-  if (
-    isFormOpen &&
-    !form.contains(e.target) &&
-    !formWrapper.classList.contains("d-none") &&
-    // !e.target.closest(".form_wrapper")
-    e.target !== openLink &&
-    e.target !== reservationBtn
-  ) {
-    closeForm();
-    console.log("click outside");
-  }
-});
-
-document.addEventListener("keydown", function (e) {
-  if (isFormOpen && e.key === "Escape") {
-    closeForm();
-  }
-});
-
-//  sprawdzenie formy
-//
-document.addEventListener("DOMContentLoaded", function () {
-  let form = document.getElementById("form");
-  // .addEventListener("submit", formSend);
-  form.addEventListener("submit", formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-    console.log(error);
-
-    let formData = new FormData(form);
-    console.log(formData);
-
-    if (error === 0) {
-      form.classList.add("_sending");
-
-      let response = await fetch("sendmail.php", {
-        method: "POST",
-        body: formData,
-      });
-      if (response.ok) {
-        let result = await response.json();
-        console.log(result);
-        alert(result.message);
-
-        form.reset();
-        form.classList.remove("_sending");
-        closeForm();
-      } else {
-        alert("blad wysylania");
-        form.classList.remove("_sending");
-      }
-    } else {
-      alert("wypelnij pola");
-    }
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll("._req");
-
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-
-      if (input.classList.contains("_email")) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
+            }
+            window.scrollTo({
+                top: goToBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
         }
-      } else if (
-        input.getAttribute("type") === "checkbox" &&
-        input.checked === false
-      ) {
-        formAddError(input);
-        error++;
-      } else {
-        if (input.value === "") {
-          formAddError(input);
-          error++;
-        }
-      }
-      if (input.classList.contains("_tel")) {
-        if (telTest(input)) {
-          formAddError(input);
-          error++;
-          console.log("error");
-        } else if (
-          input.getAttribute("type") === "checkbox" &&
-          input.checked === false
-        ) {
-          formAddError(input);
-          error++;
-        } else {
-          if (input.value === "") {
-            formAddError(input);
-            error++;
-          }
-        }
-      }
     }
+<<<<<<< HEAD
     return error;
   }
 
@@ -339,3 +153,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 >>>>>>> b60eff1 (Add:outside click on form)
+=======
+}
+>>>>>>> af022e5 (one commit)
